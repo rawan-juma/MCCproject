0251a33da570fa20ad0337b0bdae6b3e246dfcb8
dania <daniaalsaqqa2000@gmail.com>
2021-05-28 17:39:27 +0300

    commit d

--- app/src/main/java/com/example/mccproject/Activities/SignUp.kt
+++ app/src/main/java/com/example/mccproject/Activities/SignUp.kt
@@ -1,7 +1,8 @@
-package com.example.mccproject
+package com.example.mccproject.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mccproject.R
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
