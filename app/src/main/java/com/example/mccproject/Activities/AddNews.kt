ad3e76175b698fd23383f3ae80091bead9ec59d3
dania <daniaalsaqqa2000@gmail.com>
2021-05-28 17:54:58 +0300

    commit d

---
+++ app/src/main/java/com/example/mccproject/Activities/AddNews.kt
@@ -0,0 +1,35 @@
+package com.example.mccproject.Activities
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mccproject.R
+import kotlinx.android.synthetic.main.activity_add_news.*
+import java.util.*
+
+class AddNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_news)
+
+        // design Date picker "just"
+        editTextDate.setOnClickListener {
+            datePickerDialog()
+        }
+    }
+
+    fun datePickerDialog(){
+        val cldr = Calendar.getInstance()
+        val day = cldr.get(Calendar.DAY_OF_MONTH)
+        val month = cldr.get(Calendar.MONTH)
+        val year = cldr.get(Calendar.YEAR)
+// date picker dialog
+        val picker = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                editTextDate.setText(dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year)
+            }, year, month, day
+        )
+        picker.show()
+    }
+}
