5e48a4d51f7182ab04a67a76cfebbb5882a65266
rawan-juma <rawanahjuma@gmail.com>
2021-05-28 17:43:16 +0300

    first commit

--- app/src/main/java/com/example/mccproject/SignIn.kt
+++ app/src/main/java/com/example/mccproject/SignIn.kt
@@ -1,11 +1,19 @@
 package com.example.mccproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        var register=findViewById<TextView>(R.id.register)
+        register.setOnClickListener {
+            var i = Intent(this,SignUp::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/mccproject/SignUp.kt
+++ app/src/main/java/com/example/mccproject/SignUp.kt
@@ -1,11 +1,21 @@
 package com.example.mccproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUp : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        var login=findViewById<TextView>(R.id.login)
+        login.setOnClickListener {
+            var i = Intent(this,SignIn::class.java)
+            startActivity(i)
+        }
+
     }
 }
