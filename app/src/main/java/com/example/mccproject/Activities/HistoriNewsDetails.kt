a6ad92f35f7317b7c03b8b5c3d027a696f5ff86f
AlaaRaedBasal <alab0721@gmail.com>
2021-05-31 19:45:42 +0300

    first a

---
+++ app/src/main/java/com/example/mccproject/Activities/HistoriNewsDetails.kt
@@ -0,0 +1,12 @@
+package com.example.mccproject.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mccproject.R
+
+class HistoriNewsDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_histori_news_details)
+    }
+}
